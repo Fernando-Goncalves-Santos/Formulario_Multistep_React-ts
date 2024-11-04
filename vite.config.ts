cec04fcbda8625508bfc47dc0b734819d3de4360
Fernando Goncalves <fernando_goncalves_santos@hotmail.com.br>
2024-11-04 15:03:16 -0300

    ajustes na config do nome do repo

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 
 export default defineConfig({
-  base: "/Formulario_Multistep_React-ts",
+  base: "/Formulario_Multistep_React_ts",
   plugins: [react()],
 });
